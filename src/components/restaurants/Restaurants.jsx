beb799483a95a5f480253821806ddead79adc2f7
tch <tch@example.com>
2025-10-31 17:00:53 +0500

    tch

--- src/components/restaurants/Restaurants.jsx
+++ src/components/restaurants/Restaurants.jsx
@@ -1,11 +1,19 @@
 import styles from "./Restaurants.module.css";
 import RestaurantNavigationContainer from "../restaurantNavigation/RestaurantNavigationContainer";
 import RestaurantViewContainer from "../restaurantView/RestaurantViewContainer";
+import classNames from "classnames";
+import { useThemeColorContext } from "../themeColorContextProvider/ThemeColorContextProvider";
 
 export default function Restaurants() {
+  const { themeColor } = useThemeColorContext();
   return (
-    <section className={styles.container}>
-      <h2 className={styles.title}>Restaurants</h2>
+    <section
+      className={classNames(
+        styles.container,
+        styles[`container--${themeColor}`]
+      )}
+    >
+      <h2 className={classNames(styles.title, styles[`title--${themeColor}`])}>☆CubaRestaurants☆</h2>
       <RestaurantNavigationContainer />
       <RestaurantViewContainer />
     </section>
