dc02c06ac7d427f8278d5faa42eea2415a4c81f6
dn <dn@example.com>
2025-10-31 17:00:52 +0500

    dn

--- src/components/dishListItem/DishListItem.jsx
+++ src/components/dishListItem/DishListItem.jsx
@@ -1,9 +1,12 @@
+import { useThemeColorContext } from "../themeColorContextProvider/ThemeColorContextProvider";
 import styles from "./DishListItem.module.css";
+import classNames from "classnames";
 
 export default function DishListItem({ dishName, ingredients }) {
+  const { themeColor } = useThemeColorContext();
   return (
     <li className={styles.item}>
-      <h5 className={styles.title}>{dishName}</h5>
+      <h5 className={classNames(styles.title, styles[`title--${themeColor}`])}>{dishName}</h5>
       <p className={styles.ingredients}>{ingredients.join(", ")}</p>
     </li>
   );
